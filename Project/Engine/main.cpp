244863e025001e26936eb380de49e31cd077d3e7
nemunoki <nemunoki2701@gmail.com>
2026-04-19 03:39:37 +0900

    ディレクトリの階層を変更

--- Project/Engine/main.cpp
+++ Project/Engine/main.cpp
@@ -4,8 +4,6 @@
 #include <Engine/Core/Framework.h>
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-
-	// AAAA
 	
 	std::unique_ptr<Engine::Framework> game = std::make_unique<Engine::Framework>();
 	game->Run();
