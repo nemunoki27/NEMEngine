95dd5e32132ff171b03baa5da2a19144e4ca185a
nemunoki27 <nemunoki2701@gmail.com>
2026-05-27 02:35:57 +0900

    プレーンメッシュ追加とブレンド設定の修正
    BlendState.cppのアルファブレンド設定を修正し、SrcBlendAlphaをD3D12_BLEND_ONEに変更。plane.gltf/plane.binを追加し、プレーンメッシュを導入。TextureTestScene.scene.jsonでライトやメッシュ、マテリアル設定をプレーン用に更新。toneMapToView.PS.hlslに不要な括弧が追加された点も含む。

--- Project/Engine/Core/Rendering/Pipelines/Stage/BlendState.cpp
+++ Project/Engine/Core/Rendering/Pipelines/Stage/BlendState.cpp
@@ -21,7 +21,7 @@ void BlendState::Create(BlendMode blendMode, D3D12_RENDER_TARGET_BLEND_DESC& ble
 		blendDesc.SrcBlend = D3D12_BLEND_SRC_ALPHA;
 		blendDesc.DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
 		blendDesc.BlendOp = D3D12_BLEND_OP_ADD;
-		blendDesc.SrcBlendAlpha = D3D12_BLEND_ZERO;
+		blendDesc.SrcBlendAlpha = D3D12_BLEND_ONE;
 		blendDesc.DestBlendAlpha = D3D12_BLEND_ONE;
 		blendDesc.BlendOpAlpha = D3D12_BLEND_OP_ADD;
 
